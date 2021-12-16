a0a4f0a2a992abcd0f02ce1f72319865b7750070
AHMED AYMAN <a.ayman6000@gmail.com>
2021-12-16 23:55:16 +0200

    I turned out to be a loser waaa2

---
+++ sketch.js
@@ -0,0 +1,106 @@
+let bullets = [];
+let enemies = [];
+let spawningRate = 1;
+let score = 0;
+
+function setup() 
+{
+    createCanvas(600, 600);
+    spawnEnemies(5);
+}
+
+function draw() 
+{
+    background(40);
+    rectMode(CENTER);
+    fill(255);
+    noStroke();
+    
+    textSize(30);
+    textAlign(CENTER);
+  
+    text(score, 30, 40)
+  
+    // draw the player
+    circle(mouseX, mouseY, 40);
+  
+    // draw the bullets
+    for(let bullet of bullets)
+    {
+      circle(bullet.x, bullet.y, 10);
+      bullet.y -= 5;
+      if(bullet.y < 0)
+        splice(bullets.indexOf(bullet), 1);
+    }
+  
+    // draw enemies
+    for(let enemy of enemies)
+    {  
+        
+        fill(int(random(256)), 0, int(random(256)))
+        rect(enemy.x, enemy.y, 30, 20);
+        enemy.y += 1.5; 
+    }
+    fill(255);
+  
+    // collisions
+    for(let enemy of enemies)
+    {
+        for(let bullet of bullets)
+        {
+            // handle E x B collision
+            if(dist(enemy.x, enemy.y, bullet.x, bullet.y) <= 20)
+            {
+                bullets.splice(bullets.indexOf(bullet), 1);
+                enemies.splice(enemies.indexOf(enemy), 1);
+                
+                spawnEnemies(spawningRate);
+                score++;
+              
+                if(score % 20 == 0)
+                  spawningRate++;
+            }
+          
+            // handle P x E collision
+            if(dist(enemy.x, enemy.y, mouseX, mouseY) <= 10)
+              gameOver();
+            
+            // handle E x H
+            if(enemy.y >= height)
+              gameOver();
+        }
+    }
+}
+
+function gameOver()
+{
+    textSize(30);
+    textAlign(CENTER);
+    fill(255, 0, 0);
+    text("GAME OVER YA LOSER ", width/2, height/2)
+    text("YOUR SCORE : " + score, width/2, height/2 + 30)
+    noLoop();
+}
+function spawnEnemies(n)
+{
+    for(let i = 0; i < n; i++)
+    {
+        let enemy = 
+         {
+           x: random(0, width),
+           y: 0,
+           w: random(20, 60)
+         };
+        enemies.push(enemy);
+    }
+}
+function mousePressed()
+{
+    // spawn a bullet
+    let bullet = 
+        {
+          x: mouseX,
+          y: mouseY
+        };
+    bullets.push(bullet)  
+}
